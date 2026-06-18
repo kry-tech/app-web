b2d3fb270956df9592fecad94623f18cf967467f
kry-tech <anonimomods01@gmail.com>
2026-06-18 16:18:03 -0300

    Add GitHub Actions

--- app/src/main/java/com/exemplo/app/MainActivity.java
+++ app/src/main/java/com/exemplo/app/MainActivity.java
@@ -1,10 +1,16 @@
 package com.exemplo.app;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.ComponentActivity;
@@ -16,10 +22,36 @@ public class MainActivity extends ComponentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        LinearLayout splash = new LinearLayout(this);
+        splash.setOrientation(LinearLayout.VERTICAL);
+        splash.setGravity(Gravity.CENTER);
+        splash.setBackgroundColor(Color.parseColor("#121212"));
+
+        ProgressBar progressBar = new ProgressBar(this);
+        progressBar.setIndeterminate(true);
+
+        TextView text = new TextView(this);
+        text.setText("Carregando...");
+        text.setTextColor(Color.WHITE);
+        text.setTextSize(18);
+        text.setPadding(0, 30, 0, 0);
+
+        splash.addView(progressBar);
+        splash.addView(text);
+
+        setContentView(splash);
+
         WebView webView = new WebView(this);
+
+        webView.setBackgroundColor(Color.parseColor("#121212"));
 
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setHorizontalScrollBarEnabled(false);
 
         webView.setWebChromeClient(new WebChromeClient());
 
@@ -28,9 +60,14 @@ public class MainActivity extends ComponentActivity {
             "Android"
         );
 
-        webView.loadUrl("file:///android_asset/index.html");
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                setContentView(webView);
+            }
+        });
 
-        setContentView(webView);
+        webView.loadUrl("file:///android_asset/index.html");
     }
 
     public class AndroidBridge {
